0cbb3bf3b052ca776ff0891e667c4a37d3317c78
ergofriend <erg.friend@gmail.com>
2023-02-19 02:37:06 +0900

    Access-Control-Allow-Origin

--- src/utils/newResponse.ts
+++ src/utils/newResponse.ts
@@ -2,6 +2,7 @@ export const newResponse = (data: object, status = 200) =>
   new Response(JSON.stringify(data, null, 2), {
     status,
     headers: {
-      'content-type': 'application/json;charset=UTF-8',
+      'Access-Control-Allow-Origin': '*',
+      'Content-type': 'application/json',
     },
   })

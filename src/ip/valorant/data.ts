b13c642b86f4a21ccfba359fa282478f46eb751c
ergofriend <erg.friend@gmail.com>
2023-02-24 09:42:29 +0900

    add season data with Notion AI

--- src/ip/valorant/data.ts
+++ src/ip/valorant/data.ts
@@ -4,59 +4,139 @@ export const data: SeasonData = {
   1: {
     number: 1,
     name: 'IGNITION',
-    start: '',
-    end: '',
-    acts: [],
+    start: '2020-06-02T00:00:00Z',
+    end: '2021-01-11T00:00:00Z',
+    acts: [
+      {
+        number: 1,
+        start: '2020-06-02T00:00:00Z',
+        end: '2020-08-04T00:00:00Z',
+      },
+      {
+        number: 2,
+        start: '2020-08-04T00:00:00Z',
+        end: '2020-10-13T00:00:00Z',
+      },
+      {
+        number: 3,
+        start: '2020-10-13T00:00:00Z',
+        end: '2021-01-11T00:00:00Z',
+      },
+    ],
   },
   2: {
     number: 2,
     name: 'FORMATION',
-    start: '',
-    end: '',
-    acts: [],
+    start: '2021-01-12T00:00:00Z',
+    end: '2021-06-08T00:00:00Z',
+    acts: [
+      {
+        number: 1,
+        start: '2021-01-12T00:00:00Z',
+        end: '2021-03-09T00:00:00Z',
+      },
+      {
+        number: 2,
+        start: '2021-03-09T00:00:00Z',
+        end: '2021-05-11T00:00:00Z',
+      },
+      {
+        number: 3,
+        start: '2021-05-11T00:00:00Z',
+        end: '2021-06-08T00:00:00Z',
+      },
+    ],
   },
   3: {
     number: 3,
     name: 'REFLECTION',
-    start: '',
-    end: '',
-    acts: [],
+    start: '2021-06-22T00:00:00Z',
+    end: '2022-01-10T00:00:00Z',
+    acts: [
+      {
+        number: 1,
+        start: '2021-06-22T00:00:00Z',
+        end: '2021-08-24T00:00:00Z',
+      },
+      {
+        number: 2,
+        start: '2021-08-24T00:00:00Z',
+        end: '2021-11-02T00:00:00Z',
+      },
+      {
+        number: 3,
+        start: '2021-11-02T00:00:00Z',
+        end: '2022-01-10T00:00:00Z',
+      },
+    ],
   },
   4: {
     number: 4,
     name: 'DISRUPTION',
-    start: '',
-    end: '',
-    acts: [],
+    start: '2022-01-11T00:00:00Z',
+    end: '2022-06-07T00:00:00Z',
+    acts: [
+      {
+        number: 1,
+        start: '2022-01-11T00:00:00Z',
+        end: '2022-03-15T00:00:00Z',
+      },
+      {
+        number: 2,
+        start: '2022-03-15T00:00:00Z',
+        end: '2022-05-17T00:00:00Z',
+      },
+      {
+        number: 3,
+        start: '2022-05-17T00:00:00Z',
+        end: '2022-06-07T00:00:00Z',
+      },
+    ],
   },
   5: {
     number: 5,
     name: 'DIMENSION',
-    start: '',
-    end: '',
-    acts: [],
+    start: '2022-06-21T00:00:00Z',
+    end: '2023-01-09T00:00:00Z',
+    acts: [
+      {
+        number: 1,
+        start: '2022-06-21T00:00:00Z',
+        end: '2022-08-23T00:00:00Z',
+      },
+      {
+        number: 2,
+        start: '2022-08-23T00:00:00Z',
+        end: '2022-11-01T00:00:00Z',
+      },
+      {
+        number: 3,
+        start: '2022-11-01T00:00:00Z',
+        end: '2023-01-09T00:00:00Z',
+      },
+    ],
   },
   6: {
     number: 6,
     name: 'REVELATION',
-    start: '2023-01-11T00:00:00Z',
+    start: '2023-01-10T00:00:00Z',
     end: '2023-06-24T00:00:00Z',
     acts: [
       {
         number: 1,
         start: '2023-01-11T00:00:00Z',
-        end: '2023-03-30T00:00:00Z',
+        end: '2023-03-10T00:00:00Z',
       },
-      // {
-      //   number: 2,
-      //   start: '',
-      //   end: '',
-      // },
-      // {
-      //   number: 3,
-      //   start: '',
-      //   end: '',
-      // },
+      {
+        number: 2,
+        start: '',
+        end: '',
+      },
+      {
+        number: 3,
+        start: '',
+        end: '',
+      },
     ],
   },
 }

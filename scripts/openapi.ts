dead03b9db6afdc06f72e55ea0d28e5495772666
ergofriend <erg.friend@gmail.com>
2023-02-26 20:29:02 +0900

    refactor deploy to bump.sh

--- scripts/openapi.ts
+++ scripts/openapi.ts
@@ -2,4 +2,12 @@ import * as fs from 'fs'
 
 import { schema } from '../src/schema'
 
-fs.writeFileSync('./public-api.json', JSON.stringify(schema, null, 2))
+const path = process.argv[2]
+
+if (!path || !path.endsWith('.json')) {
+  console.log(`path: ${path}`)
+  console.error('Please provide a path to the schema json file.')
+  process.exit(1)
+}
+
+fs.writeFileSync(path, JSON.stringify(schema, null, 2))

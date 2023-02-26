c63a6905d5cca70a02cbedfd28fb6d4ad927f78f
ergofriend <erg.friend@gmail.com>
2023-02-26 19:08:10 +0900

    fix import

--- scripts/openapi.ts
+++ scripts/openapi.ts
@@ -1,9 +1,5 @@
 import * as fs from 'fs'
 
-import { router } from '../src/index'
+import { schema } from '../src/schema'
 
-// Get the Schema from itty-router-openapi
-const schema = router.schema
-
-// Write the final schema
 fs.writeFileSync('./public-api.json', JSON.stringify(schema, null, 2))

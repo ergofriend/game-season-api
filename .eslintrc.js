cb78dee84e7216df58de2f319679e7df5d64aa79
ergofriend <erg.friend@gmail.com>
2023-03-12 13:17:48 +0900

    add lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,14 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  extends: ['prettier', 'eslint:recommended'],
-  overrides: [
-    {
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-      ],
-      files: ['**/*.ts', '**/*.tsx'],
-      parserOptions: {
-        tsconfigRootDir: __dirname,
-        project: [
-          './tsconfig.json',
-          './apps/*/tsconfig.json',
-          './packages/*/tsconfig.json',
-        ],
-      },
-      rules: {
-        '@typescript-eslint/no-unused-vars': [
-          'error',
-          {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-            caughtErrorsIgnorePattern: '^_',
-          },
-        ],
-        '@typescript-eslint/require-await': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-      },
-    },
-  ],
-  root: true,
-  reportUnusedDisableDirectives: true,
-  ignorePatterns: ['.eslintrc.js'],
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
 }
 
 module.exports = config
